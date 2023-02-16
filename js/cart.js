526193c58918c40d79f3831421a22fe72f019929
BlckTitan <dorshman406@gmail.com>
2023-02-16 22:10:13 +0100

    Added layout for the payment page

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,37 @@
-let cartItems = document.querySelector('section.cartItems');
+let cart_items = document.querySelector('section.cartItems');
 let payment = document.querySelector('section.payment');
-let checkoutSuccess = document.querySelector('section.checkoutSuccess');
+let checkout_success = document.querySelector('section.checkoutSuccess');
+let to_checkout = document.querySelector('button.toCheckout');
+let step_order = document.querySelector('span.order');
+let step_payment = document.querySelector('span.payment');
+let step_success = document.querySelector('span.success');
 
-const checkoutStepsDisplay = () =>{
-    cartItems.style.visibility = 'visible';
-    payment.style.visibility = 'hidden';
-    checkoutSuccess.style.visibility = 'hidden';
+const checkoutStepsDisplay = (step) =>{
+    if (step == 'payment') {
+        step_order.classList.remove('active');
+        step_payment.classList.add('active');
+        cart_items.style.visibility = 'hidden';
+        payment.style.visibility = 'visible';
+    } else if(step == 'success'){
+        step_success.classList.add('active');
+        checkout_success.style.visibility = 'visible';
+    }else{
+        step_order.classList.add('active');
+        cart_items.style.visibility = 'visible';
+    }
 }
-const messageDisplay = () =>{
-    let displayMessage = document.querySelector('.displayMessage');
+const selectStep = () =>{
+    step_order.classList.add('active');
+    cart_items.style.visibility = 'visible';
+    to_checkout.addEventListener('click', ()=>checkoutStepsDisplay('payment'))
+    //to_checkout.addEventListener('click', checkoutStepsDisplay('payment'))
+    //to_checkout.addEventListener('click', checkoutStepsDisplay('payment'))
+}
+/*const messageDisplay = () =>{
+    let display_message = document.querySelector('.displayMessage');
     setTimeout(()=>{
-        displayMessage.style.visibility = 'hidden'
+        display_message.style.visibility = 'hidden'
     }, 1000)
-}
-messageDisplay()
-checkoutStepsDisplay()
+}*/
+//messageDisplay()
+selectStep()
