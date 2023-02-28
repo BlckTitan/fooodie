7b9ae123b0b63cd1bb3e99cf29f736957c8698fe
BlckTitan <dorshman406@gmail.com>
2023-02-28 17:34:41 +0100

    Modified the css and made a css file out of the grill section css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 const carouselImages = [
     {id: 1, imgLink: 'https://images.unsplash.com/photo-1558030089-02acba3c214e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'},
     {id: 2, imgLink: 'https://images.unsplash.com/photo-1558030137-a56c1b004fa3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'},
-    {id: 3, imgLink: 'https://images.unsplash.com/photo-1558030006-450675393462?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'}
+    {id: 3, imgLink: 'https://images.unsplash.com/photo-1558030006-450675393462?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'},
+    {id: 4, imgLink: 'https://images.unsplash.com/photo-1558030089-02acba3c214e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'},
 ]
 const arrowTop = () =>{
     window.scrollTo({
         top:0
     })
 }
+const carouselTimer = () =>{
+    let counter = 1
+    
+    setInterval(()=>{
+
+        if(counter >= carouselImages.length || counter < 1){
+            counter = 1;
+        }else{
+            counter++
+            carousel(counter)
+        }
+        
+    }, 6000);
+}
 const carousel = (currentImg = 1) =>{
-   
+
     const carouselDisplay = document.querySelector('section.grills');
 
     carouselImages.map(img => (
@@ -19,32 +34,5 @@ const carousel = (currentImg = 1) =>{
     ))
 
 }
-const carouselTimer = ( navigate = 0) =>{
-    let counter = 1
-    
-    setInterval(()=>{
-
-        if(counter >= carouselImages.length || counter < 1){
-            counter = 1
-            
-        }else{
-            //(navigate != '') ? eval(counter + navigate) : counter++
-            counter++
-            carousel(counter)
-            console.log(eval(counter+navigate))
-        }
-        
-    }, 6000);
-}
-const carouselNavLeft = () =>{
-    let leftBtn = document.querySelector('button.btnLeft');
-    leftBtn.addEventListener('click', carouselTimer(-1))
-}
-const carouselNavRight = () =>{
-    let rightBtn = document.querySelector('button.btnRight');
-    rightBtn.addEventListener('click', carouselTimer(+1))
-}
-carouselNavLeft()
-carouselNavRight()
 carouselTimer()
 carousel()
