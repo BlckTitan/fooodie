28a7d46d59ba35ddf5dfa5d6da5ebd70db50e9a7
BlckTitan <dorshman406@gmail.com>
2024-09-20 20:35:42 +0100

    feat(avatar): This commit implements a feature where images uploaded by users are stored in the public/img directory, enhancing user experience by providing a centralized location for easy image access.

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,11 +1,18 @@
+'use server'
+import fs from 'fs/promises';
+import path from 'path';
 
 export async function POST(req){
 
     const data = await req.formData()
-    // const body = await req.json()
+    const file = data.get('file');
+    const url = data.get('url')
 
-    if(data.get('files')){
-        console.log('we have a file')
+    // console.log(file.size)
+
+    if(data.get('file')){
+        //send a copy of the uploaded file to be saved in public/img dir
+        await saveFilesToLocal(file)
     }else{
         console.log('no file')
     }
@@ -13,3 +20,21 @@ export async function POST(req){
     return Response.json(true)
     
 }
+
+//save files to local
+const saveFilesToLocal = async (formData) =>{
+    const file = formData
+    // const url = formData.get('url')
+
+    const bufferPromise = file.arrayBuffer()
+      .then(data => {
+        //creating a buffer file from the upload image
+        const buffer = Buffer.from(data)
+
+        //creating file upload path
+        const uploadDir = path.join(process.cwd(), 'public/img', `/${file.name}`)
+        // uploading file to directory
+        fs.writeFile(uploadDir, buffer)
+    })
+
+  }
--- src/components/layout/profile/Avatar.jsx
+++ src/components/layout/profile/Avatar.jsx
@@ -7,6 +7,8 @@ import { Form } from 'react-bootstrap';
 import { BsPlusLg } from 'react-icons/bs';
 import axios from 'axios';
 import { CldUploadWidget } from 'next-cloudinary';
+import { FiEdit, FiExternalLink } from 'react-icons/fi';
+import { toast } from 'react-toastify';
 
 export default function Avatar() {
 
@@ -14,12 +16,37 @@ export default function Avatar() {
   const session = useSession()
   const userData = session?.data?.user
 
-  const handleUpload = (e) =>{
-    const files = e.target.files
+  // const handleUpload = (e) =>{
+  //   const files = e.target.files
 
-    if(files.length > 0){
-      const data = new FormData;
-      data.set('files', files[0])
+  //   if(files.length > 0){
+  //     const data = new FormData;
+  //     data.set('files', files[0])
+
+  //     axios({
+  //       method: 'post',
+  //       url: '/api/upload/',
+  //       data: data,
+  //       // headers: {'Content-Type': 'multipart/form-data'} 
+  //     });
+  //   }
+  // }
+
+  
+
+  //handle upload using api
+  const handleUpload = async (formData) => {
+    const file = formData.target.files
+    
+    //checking for file size and type(image files)
+    if(file[0].size < 1024 * 1024 && file[0].type.startsWith('image/')){
+
+      //creating a link for the upload file
+      const url = URL.createObjectURL(file[0])
+
+      const data = new FormData; 
+      data.set('file', file[0])
+      data.set('url', url)
 
       axios({
         method: 'post',
@@ -27,7 +54,15 @@ export default function Avatar() {
         data: data,
         // headers: {'Content-Type': 'multipart/form-data'} 
       });
+
+      // return file 
+      // setImg(file[0])
+
+      return toast.success('Profile photo successfully updated', {position: 'top-right', autoClose: 3000, toastId: 1});
+    }else{
+      return toast.error('Image file too large', {position: 'top-right', autoClose: 3000, toastId: 1});
     }
+
   }
 
   return (
@@ -55,7 +90,7 @@ export default function Avatar() {
             layout='contain'
         />
       }
-        {/* <Form className='flex justify-between'>
+        <Form className='flex justify-between' action={handleUpload}>
           <Form.Group className="mb-3" controlId="formBasicText">
 
               <Form.Label 
@@ -68,29 +103,59 @@ export default function Avatar() {
               <Form.Control 
                 id='uploadAvatar'
                 type='file'
+                accept='image/*'
                 // value={img}
                 onChange={handleUpload}
                 className='hidden'
+                name='image'
               />
           </Form.Group>
-        </Form> */}
+        </Form>
          <div>
             
 
-            {/* <CldUploadWidget uploadPreset="fooodie_food_ordering_app"> */}
-            <CldUploadWidget signatureEndpoint='/api/sign-image'>
-              {({ open }) => {
-                return (
-                  <button 
-                   className='text-xl xl:text-2xl cursor-pointer absolute border text-gray-500 -right-2.5 xl:-right-0.5 -bottom-1.5 bg-white rounded-full'
-                    onClick={() => open()}>
-                    <BsPlusLg />
-                  </button>
-                );
-              }}
-            </CldUploadWidget>
+          {/* <CldUploadWidget uploadPreset="fooodie_food_ordering_app"> */}
+          {/* <CldUploadWidget signatureEndpoint='/api/sign-image'>
+            {({ open }) => {
+              return (
+                <button 
+                  className='text-xl xl:text-2xl cursor-pointer absolute border text-gray-500 -right-2.5 xl:-right-0.5 -bottom-1.5 bg-white rounded-full'
+                  onClick={() => open()}>
+                  <BsPlusLg />
+                </button>
+              );
+            }}
+          </CldUploadWidget> */}
 
           </div>
     </div>
   )
 }
+
+// const { resources: sneakers } = await cloudinary.api.resources_by_tag('nextjs-server-actions-upload-sneakers', { context: true });
+
+//     // 'use server'
+
+//     const file = formData.get('image');
+//     const arrayBuffer = await file.arrayBuffer();
+//     const buffer = Buffer.from(arrayBuffer);
+
+//     await new Promise((resolve, reject) => {
+
+//       cloudinary.uploader.upload_stream({
+//         tags: ['nextjs-server-actions-upload-sneakers'],
+//         upload_preset: 'fooodie_food_ordering_app'
+//       }, function (error, result) {
+
+//         if (error) {
+//           reject(error);
+//           return;
+//         }
+
+//         resolve(result);
+
+//       })
+//       .end(buffer);
+//     });
+
+//     revalidatePath('/')
